ef969948339e339f0cb23d607a9f863528d23ef4
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2026-06-12 00:22:18 -0500

    chore: upd dto

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -1,6 +1,12 @@
-import { IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 export class ForgotPasswordDto {
+  @IsString()
   @IsEmail()
+  @ApiProperty({
+    description: 'User email',
+    example: 'email@gmail.com',
+  })
   email: string;
 }
--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -1,13 +1,41 @@
-import { IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class ResetPasswordDto {
   @IsString()
+  @IsEmail()
+  @ApiProperty({
+    description: 'User email',
+    example: 'email@gmail.com',
+  })
   email: string;
 
   @IsString()
+  @Matches(/^\d{6}$/, {
+    message: 'The verification code must contain exactly 6 digits',
+  })
+  @ApiProperty({
+    description: 'Code OPT',
+    example: '123456',
+  })
   code: string;
 
   @IsString()
   @MinLength(6)
+  @MaxLength(50)
+  @ApiProperty({
+    description: 'User password',
+    example: '********',
+  })
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
   password: string;
 }
